2937cf90096d572b609bf0099f15d95dd485305b
hoBahk <qudgh849@gmail.com>
2022-07-28 14:05:55 +0900

    init

---
+++ GitHubActionsTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitHubActionsTest
+//
+//  Created by 박병호 on 2022/07/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitHubActionsTest/GitHubActionsTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubActionsTestApp.swift
+//  GitHubActionsTest
+//
+//  Created by 박병호 on 2022/07/28.
+//
+
+import SwiftUI
+
+@main
+struct GitHubActionsTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
